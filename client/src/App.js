514d11d71fd9c0088b6f91fa58d2f13fd2498e43
Lambda_School_Loaner_229 <lambda_school_loaner_229@Lambda-School-Loaner-229s-MacBook-Pro.local>
2020-02-24 22:07:03 -0700

    Fixed PrivateRoute to redirect to root '/' instead of '/login'.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import Login from "./components/Login";
 import PrivateRoute from './components/PrivateRoute';
 import BubblePage from './components/BubblePage';
-
-
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Login} />
         <PrivateRoute path='/user' component={BubblePage} />
       </div>
     </Router>
